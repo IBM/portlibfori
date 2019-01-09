0bfed91511f34351b7ec52b3e164b6acf4557722
Jesse G <jgorzinski@gmail.com>
2019-01-09 19:50:27 +0000

    add extern "C" for execinfo.h

--- execinfo.h
+++ execinfo.h
@@ -3,8 +3,14 @@
 
 #include <stdlib.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 size_t backtrace(void** buffer, size_t size);
 
 char** backtrace_symbols(void* const* frames, size_t size);
 
+#ifdef __cplusplus
+}
+#endif
 #endif
