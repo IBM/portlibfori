48801eed2fb73aac487b796fb301516eb875ce2e
Kevin Adler <kadler@us.ibm.com>
2020-03-12 15:41:48 -0500

    feat: Add backtrace_symbols_fd
    Move symbol generation in to its own function. This seems rather
    superfluous right now, but will be more important once we start reading
    debug info.
    
    Fixes #3

--- util/execinfo.h
+++ util/execinfo.h
@@ -10,6 +10,8 @@ size_t backtrace(void** buffer, size_t size);
 
 char** backtrace_symbols(void* const* frames, size_t size);
 
+void backtrace_symbols_fd(void *const *frames, size_t count, int fd);
+
 #ifdef __cplusplus
 }
 #endif
