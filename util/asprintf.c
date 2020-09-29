caae367d4ca96c3d936813322e2873e12a0b206a
naveen ram <naveenaram@gmail.com>
2020-09-29 09:44:38 -0500

    Apply review changes

--- util/asprintf.c
+++ util/asprintf.c
@@ -2,39 +2,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int 
-vasprintf(char **ret, const char *format, va_list args)
+int libutil_vasprintf(char **ret, const char *format, va_list args)
 {
     *ret = NULL;
-    
-    va_start(args, format);
-    int size = vsnprintf(NULL, 0, format, args);
-    va_end(args);
 
-    if (size == -1)
-        return size;
+    va_list args_copy;
+    va_copy(args_copy, args)
+    int size = vsnprintf(NULL, 0, format, args);
+    va_end(args_copy)
+
+    if (size == -1) return size;
     
     char* str = malloc(size+1);
-    if (str == NULL)
-        return -1;
+    
+    if (str == NULL) return -1;
 
-    va_start(args, format);
     size = vsnprintf(str, (size_t) size + 1, format, args);
-    va_end(args);   
     
     if (size == -1)
+    {
         free(str);
-    
+        return -1
+    }
+
     *ret = str;
 
     return size;
 }
 
-int 
-asprintf(char **ret, const char *format, ...)
+int libutil_asprintf(char **ret, const char *format, ...)
 {
   int r;
   va_list args;
+  va_start(args, format);
   r = vasprintf(ret, format, args);
-  return(r);
+  va_end(args);
+  return r;
 }
