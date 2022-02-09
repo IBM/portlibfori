c3cf23cf1c43af40993d2d14bdaea6d8b7c0551b
Kevin Adler <kadler@us.ibm.com>
2022-02-09 10:34:58 -0600

    Update util/wrapper/stdlib.h

--- util/wrapper/stdlib.h
+++ util/wrapper/stdlib.h
@@ -9,6 +9,8 @@
 extern "C" {
 #endif
 
+const char* libutil_getprogname(void);
+void libutil_setprogname(const char *);
 const char* getprogname(void) __asm__ ("libutil_getprogname");
 void setprogname(const char *) __asm__ ("libutil__setprogname");
 
